cada2d129852af1fbc2b75a8cd81c6ac3c9e90e6
kimorkim <kimorkim@gmail.com>
2016-01-17 21:24:31 +0900

    hotfix 클론 떠서 기존데이터랑 램던 데이터 연결고리 끊기

--- src/sdm.js
+++ src/sdm.js
@@ -36,7 +36,7 @@
 			});
 
 			if(game_type === 'random') {
-				var random_game = game_data;
+				var random_game = objectClone(game_data);
 				var value = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80']
 				var end = 30;
 				var start = 15;
@@ -46,8 +46,7 @@
 					var pick_idx = Math.floor(Math.random() * value.length);
 					var coord = getCoordByCount(value[pick_idx]);
 					try{
-
-					random_game[coord.i][coord.j][coord.k] = undefined;
+						random_game[coord.i][coord.j][coord.k] = undefined;
 					} catch(e) {
 						console.error(coord);
 						console.error(value[pick_idx]);
@@ -82,7 +81,11 @@
 				}
 			}
 			return result;
-		};
+		}
+
+		var objectClone = function (object) {
+			return JSON.parse(JSON.stringify(object));
+		}
 
 		var getCoordByCount = function (count) {
 			var _i = Math.floor((count - 1) / 9);
