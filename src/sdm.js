27e4dca14702b64e96668f7794df3a64926f8920
kimorkim <kimorkim@gmail.com>
2016-01-17 20:55:39 +0900

    random 게임 데이터 추가

--- src/sdm.js
+++ src/sdm.js
@@ -25,7 +25,7 @@
 			initRndNum();
 		}
 
-		my.getGameData = function(type) {
+		my.getGameData = function(game_type) {
 			initRndNum();
 			var number = Math.floor((Math.random() * _GAME.length));
 			var type = _GAME[number];
@@ -35,7 +35,7 @@
 				game_data.push(moveCard(_target, _PATTERN[data]));
 			});
 
-			if(type === 'random') {
+			if(game_type === 'random') {
 				var random_game = game_data;
 				var value = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80']
 				var end = 30;
@@ -45,7 +45,13 @@
 				for(var i = 0; i < pick_cnt; i++) {
 					var pick_idx = Math.floor(Math.random() * value.length);
 					var coord = getCoordByCount(value[pick_idx]);
+					try{
+
 					random_game[coord.i][coord.j][coord.k] = undefined;
+					} catch(e) {
+						console.error(coord);
+						console.error(value[pick_idx]);
+					}
 					value.splice(pick_idx, pick_idx++);
 				}
 
@@ -80,9 +86,9 @@
 
 		var getCoordByCount = function (count) {
 			var _i = Math.floor((count - 1) / 9);
-			var _j = Math.floor(((count % 9) - 1) / 3);
-			var _k = Math.floor(((count % 9) - 1) % 3);
-			
+			var _j = Math.floor((count % 9) / 3);
+			var _k = Math.floor((count % 9) % 3);
+
 			return {
 				i: _i,
 				j: _j,
