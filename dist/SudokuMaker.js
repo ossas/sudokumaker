77da1e39b672a177784b6e299d3acd3d44f932b6
jykim <kimorkim@gmail.com>
2016-09-14 20:50:54 +0900

    update name

--- dist/SudokuMaker.js
+++ dist/SudokuMaker.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["SudokuMaker"] = factory();
+		exports["sudokuMaker"] = factory();
 	else
-		root["SudokuMaker"] = factory();
+		root["sudokuMaker"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
